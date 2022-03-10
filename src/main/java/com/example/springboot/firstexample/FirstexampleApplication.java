014bf93553b84c04bf4a887dade9a00c942b589b
CoolSantoshRao <coolsantoshrao@gmail.com>
2022-03-10 09:56:25 +0530

    Second Commit

--- src/main/java/com/example/springboot/firstexample/FirstexampleApplication.java
+++ src/main/java/com/example/springboot/firstexample/FirstexampleApplication.java
@@ -1,13 +1,23 @@
 package com.example.springboot.firstexample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FirstexampleApplication {
+	
+	private static Logger logger=LoggerFactory.getLogger(FirstexampleApplication.class);
+	
+	
 
 	public static void main(String[] args) {
+		logger.info("Before Execute Main");
 		SpringApplication.run(FirstexampleApplication.class, args);
+		logger.info("Execute Main");
+	
 	}
 
 }
