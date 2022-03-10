c23d96e14858e557bf54d8611d20be1847a476cc
CoolSantoshRao <coolsantoshrao@gmail.com>
2022-03-10 12:57:35 +0530

    Check the success email notification

--- src/test/java/com/example/springboot/firstexample/FirstexampleApplicationTests.java
+++ src/test/java/com/example/springboot/firstexample/FirstexampleApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FirstexampleApplicationTests {
 	
-
+public static Logger logger=LoggerFactory.getLogger(FirstexampleApplicationTests.class);
 	@Test
 	  void contextLoads() {
 		logger.info("Test Case Executing....");
