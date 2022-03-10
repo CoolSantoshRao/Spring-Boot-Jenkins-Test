4e60d72e46965741b83e0a9491d178a233aa37b1
CoolSantoshRao <coolsantoshrao@gmail.com>
2022-03-10 10:13:41 +0530

    3rd commit

--- src/test/java/com/example/springboot/firstexample/FirstexampleApplicationTests.java
+++ src/test/java/com/example/springboot/firstexample/FirstexampleApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.springboot.firstexample;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FirstexampleApplicationTests {
+	private static Logger logger=LoggerFactory.getLogger(FirstexampleApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test Case Executing");
+		assertEquals(true, true);
+		
 	}
 
 }
